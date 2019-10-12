911222387ffd42ed767ec6c7b7e4f1aee99838c0
硫酸 <lsch001@163.com>
2019-10-12 17:30:16 +0800

    :pencil: Fixing Typos

--- src/assets/js/states/PixelStretch.js
+++ src/assets/js/states/PixelStretch.js
@@ -14,10 +14,10 @@ class PixelStretch {
             antialias: true,
             preserveDrawingBuffer: true,
         });
-        this.app=app;
+        this.app = app;
         document.body.appendChild(app.view);
         // PIXI.utils.clearTextureCache;
-        app.loader.add('pic1', `/assets/img/pic${Math.floor(Math.random()*5+1)}.jpg`)
+        app.loader.add('pic1', `./assets/img/pic${Math.floor(Math.random()*5+1)}.jpg`)
         app.loader.load(onLoaded.bind(this))
 
         function onLoaded(loader, resources) {
@@ -25,36 +25,38 @@ class PixelStretch {
             app.stage.addChild(this.pic)
 
             // uniform float boundary;
-            // uniform float hDir;
-            var stretchfilter = new PixelStretchFilter(0.0, false)
+            // uniform float verticalDir;
+            var stretchfilter = new PixelStretchFilter(0, false)
             app.stage.filters = [stretchfilter]
 
             var gui = new dat.GUI();
-            gui.add(stretchfilter, 'boundary', -1, 1);
+            gui.add(stretchfilter, 'boundary').min(-1).max(1).step(0.01);
             gui.add(stretchfilter, 'verticalDir').name('verticalDir');
-            window.addEventListener('resize',this.handleResize.bind(this))
+
+            window.addEventListener('resize', this.handleResize.bind(this))
             this.handleResize();
-            var count = 0;
-            app.ticker.add(function() {
-                // count += 0.01;
-                // stretchfilter.boundary = Math.abs(Math.sin(count))
-            })
+
+            // var count = 0;
+            // app.ticker.add(function() {
+            // count += 0.01;
+            // stretchfilter.boundary = Math.abs(Math.sin(count))
+            // })
         }
 
     }
 
     handleResize() {
-        var w=window.innerWidth;
-        var h= window.innerHeight;
-        var pic= this.pic;
-        var s=pic.width/pic.height;
-        this.app.renderer.resize(w,h);
-        if(s>w/h){
-            pic.width=w;
-            pic.height=w/s;
-        }else{
-            pic.height=h;
-            pic.width=h*s;
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        var pic = this.pic;
+        var s = pic.width / pic.height;
+        this.app.renderer.resize(w, h);
+        if (s > w / h) {
+            pic.width = w;
+            pic.height = w / s;
+        } else {
+            pic.height = h;
+            pic.width = h * s;
         }
 
     }
